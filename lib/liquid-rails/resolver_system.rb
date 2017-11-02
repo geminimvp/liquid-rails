28327f115a6a9f14c46dd78b7d2472d8f75f7c38
fastjames <fastjames@gmail.com>
2017-11-02 16:45:56 -0500

    fixup
    update ResolverSystem to work with Liquid 4
    
    Since Liquid 4 doesn't pass in a context to the file system any more, we
    build our own context and pass it in. The important thing we need to
    know is what controller the call is coming from, since that affects our
    lookup path for templates.
    
    I'm not happy that we have to add a parameter to the regular FileSystem
    signature, but I don't see a way around it.

--- lib/liquid-rails/resolver_system.rb
+++ lib/liquid-rails/resolver_system.rb
@@ -3,23 +3,37 @@
 module Liquid
   module Rails
     class ResolverSystem
-      # Return a valid liquid template string for requested partial path
-      def read_template_file(template_path, context)
-        controller = context.registers[:controller]
 
+      attr_accessor :context
+
+      def initialize(context)
+        self.context = context
+        super()
+      end
+
+      # Return a valid liquid template string for requested partial path
+      def read_template_file(template_path)
         controller_path = controller.controller_path
         template_path   = "#{controller_path}/#{template_path}" unless template_path.include?('/')
 
         name = template_path.split('/').last
         prefix = template_path.split('/')[0...-1].join('/')
 
-        result = controller.lookup_context.find_all(name, prefix, true)
+        result = lookup_context.find_all(name, prefix, true)
 
         if result.present?
           result.first.source.force_encoding("UTF-8") # cast to utf8 as it was getting encoding errors
         else
           raise FileSystemError, "No such template '#{template_path}'"
         end
+      end
+
+      def controller
+        context.registers[:controller]
+      end
+
+      def lookup_context
+        controller.lookup_context
       end
     end
   end
