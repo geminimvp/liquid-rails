569a1de8d9b3df656c1fdd3382245c2390005629
spinningtopsofdoom + Valerie Burzynski <dev+spinningtopsofdoom+vburzynski@bendyworks.com>
2018-03-28 11:15:38 -0500

    cleanup

--- lib/liquid-rails/resolver_system.rb
+++ lib/liquid-rails/resolver_system.rb
@@ -3,35 +3,19 @@
 module Liquid
   module Rails
     class ResolverSystem
-      # @param root [String] the root path where all templates may be found
-      def initialize(root='')
-        @root = root
-      end
-
-      # Return a valid liquid template string for requested partial path
-      # @param template_path [String] the path to the templates
-      # @param context
       def read_template_file(template_path, context)
         controller = context.registers[:controller]
-        full_path = full_path(template_path, controller)
+        controller_path = controller.controller_path
+        template_path = "#{controller_path}/#{template_path}" unless template_path.include?('/')
 
-        name   = full_path.split('/').last
-        prefix = full_path.split('/')[0...-1].join('/')
+        name   = template_path.split('/').last
+        prefix = template_path.split('/')[0...-1].join('/')
 
         result = controller.lookup_context.find_all(name, prefix, true)
-        raise FileSystemError, "No such template '#{full_path}'" unless result.present?
+        raise FileSystemError, "No such template '#{template_path}'" unless result.present?
 
         # cast to utf8 as it was getting encoding errors
         result.first.source.force_encoding("UTF-8")
-      end
-
-      private
-
-      # Join the root with the template file path
-      def full_path(template_path, controller)
-        controller_path = controller.controller_path
-        template_path = "#{controller_path}/#{template_path}" unless template_path.include?('/')
-        File.join(root, File.dirname(template_path))
       end
     end
   end
